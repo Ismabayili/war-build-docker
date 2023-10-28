4ee2d75457a17bca29f4eb39f7f9d1da2c970730
Ismabayili <135331260+Ismabayili@users.noreply.github.com>
2023-10-28 20:37:12 +0200

    Update run.sh wordpress 28 octobre

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-docker run -d --name webapp -p 8081:8080 \
+docker run -d --name wordpress -p 81:80 \
 -v $(pwd)/target/hello-world-war-1.0.0.war:/usr/local/tomcat/webapps/ROOT.war \
 -v $(pwd)/target/hello-world-war-1.0.0/:/usr/local/tomcat/webapps/ROOT \
 tomcat
+
+#docker run -d --name webapp -p 8081:8080 \
+#-v $(pwd)/target/hello-world-war-1.0.0.war:/usr/local/tomcat/webapps/ROOT.war \
+#-v $(pwd)/target/hello-world-war-1.0.0/:/usr/local/tomcat/webapps/ROOT \
+#tomcat

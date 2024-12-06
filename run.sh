34d1b1b0fa378105b4faa5880accfccee14b4456
Ismabayili <135331260+Ismabayili@users.noreply.github.com>
2024-12-06 23:28:35 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker run -d --name wordpress -p 81:80 \
+docker run -d --name wordpress -p 80:80 \
 -v $(pwd)/target/hello-world-war-1.0.0.war:/usr/local/tomcat/webapps/ROOT.war \
 -v $(pwd)/target/hello-world-war-1.0.0/:/usr/local/tomcat/webapps/ROOT \
 tomcat
